062f15aa4ec5a2e3e22ef78c4ef003525e79cf09
Ajay Patel <ap1892503@gmail.com>
2025-12-20 15:32:01 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello",Ajay pate name)
+
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
+
+print("Numbers from 1 to 5:")
+for i in range(1, 6):
+    print(i)
